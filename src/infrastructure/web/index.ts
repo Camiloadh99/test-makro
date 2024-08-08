26f46662741e5b69b3707a07e0e7c9c5eb342156
Camilo <Camilo>
2024-08-08 12:40:14 -0500

    feat/ express static display

--- src/infrastructure/web/index.ts
+++ src/infrastructure/web/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import appRouter from './routes';
+import path from 'path';
 
 const createServer = () => {
   const app = express();
@@ -8,6 +9,8 @@ const createServer = () => {
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
+
+  app.use(express.static(path.join(__dirname, './../../frontend')));
 
   app.use('/api', appRouter);
 
