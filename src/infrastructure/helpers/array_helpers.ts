87c2c2c50bebdd39bcb0d85f142c44a5a54f7e6c
Camilo <Camilo>
2024-08-22 15:16:01 -0500

    feat/ merge codes and units avaiables

--- src/infrastructure/helpers/array_helpers.ts
+++ src/infrastructure/helpers/array_helpers.ts
@@ -6,10 +6,13 @@ export const findIndexOnMatrix = (matrix: RowData[][], arrayToFind: string[]): n
     return innerArray.some((item) => arrayToFind.includes(`${item}`));
   });
 };
+export const deleteLineBreakString = (str: string) => {
+  return str.replace(/(\r\n|\n|\r)/gm, ' ') || '';
+};
 
 export const convertRowDataToStringArray = (rowData: RowData[]): string[] => {
   if (!rowData) return [];
-  return Object.values(rowData).map((value) => `${value}`);
+  return Object.values(rowData).map((value) => deleteLineBreakString(`${value}`));
 };
 
 export const convertRowDataToNumberArray = (rowData: RowData[]): number[] => {
@@ -19,4 +22,15 @@ export const convertRowDataToNumberArray = (rowData: RowData[]): number[] => {
 export const getColorForNumber = (num: number) => {
   const index = (num - 1) % COLORS.length;
   return COLORS[index] || 'FFFFFF'; // Blanco por defecto
+};
+
+export const uppercaseFirstLetters = (str: string) => {
+  const letters = str.toLocaleLowerCase().split(' ');
+  if (letters.length === 0) return '';
+  if (!str) return '';
+  return letters
+    .map((letter) => {
+      return letter[0]?.toUpperCase() + letter?.substring(1);
+    })
+    .join(' ');
 };
