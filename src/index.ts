c2fecb88d12b49a2d9d838ab28ea788a36a0c7e6
Camilo <Camilo>
2024-08-23 11:14:58 -0500

    se express sin funcion

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import createServer from './infrastructure/web';
+import express from 'express';
+import cors from 'cors';
+import appRouter from './infrastructure/web/routes';
+import path from 'path';
+import { buildOpenApiMiddelware } from '@fnd/libs/open-api';
+import { swaggerDocument, swaggerUi } from '@fnd/libs/swagger-ui';
 
-const runApp = async () => {
-  createServer();
-  console.log('Server is running');
-};
+const app = express();
 
-runApp();
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/spec', express.static('./oas3.yaml'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(buildOpenApiMiddelware());
+
+app.use(express.static(path.join(__dirname, './../../frontend')));
+
+app.use('/api', appRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
