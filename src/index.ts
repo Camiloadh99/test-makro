67d03468cba9c6ca37284b67acc1e25d7685d60d
Camilo <Camilo>
2024-08-23 11:31:20 -0500

    adding vercel version

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import appRouter from './infrastructure/web/routes';
-import path from 'path';
-import { buildOpenApiMiddelware } from '@fnd/libs/open-api';
-import { swaggerDocument, swaggerUi } from '@fnd/libs/swagger-ui';
+import createServer from './infrastructure/web';
 
-const app = express();
+const runApp = async () => {
+  createServer();
+  console.log('Server is running');
+};
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.use('/spec', express.static('./oas3.yaml'));
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(buildOpenApiMiddelware());
-
-app.use(express.static(path.join(__dirname, './../../frontend')));
-
-app.use('/api', appRouter);
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+runApp();
