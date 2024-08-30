ba02f995e19b9838b9de49727baecfd242e325cc
Camilo <Camilo>
2024-08-30 13:48:47 -0500

    feat docker on

--- src/domain/errors/base_error.ts
+++ src/domain/errors/base_error.ts
@@ -1,19 +1,19 @@
 export class BaseError extends Error {
-    status: number;
-    _stack: any;
-    error: string;
-    metadata: any;
+  status: number;
+  _stack: any;
+  error: string;
+  metadata: any;
 
-    constructor(message: string, status: number, metadata?: any) {
-        super(message || 'Default error');
-        this.error = this.message;
-        this.status = status;
-        this._stack = this.stack;
-        this.metadata = metadata;
-    }
+  constructor(message: string, status: number, metadata?: any) {
+    super(message || 'Default error');
+    this.error = this.message;
+    this.status = status;
+    this._stack = this.stack;
+    this.metadata = metadata;
+  }
 
-    toString() {
-        const metadata = this.metadata ? '\n Metadata: ' + this.metadata : '';
-        return super.toString() + metadata;
-    }
+  toString() {
+    const metadata = this.metadata ? '\n Metadata: ' + this.metadata : '';
+    return super.toString() + metadata;
+  }
 }
--- src/domain/errors/storage_error.ts
+++ src/domain/errors/storage_error.ts
@@ -3,7 +3,7 @@ import { BaseError } from './base_error';
 type Status = 500 | 404;
 
 export class StorageError extends BaseError {
-    constructor(message: string, status: Status = 500, metatada?: any) {
-        super(message, status, metatada);
-    }
+  constructor(message: string, status: Status = 500, metatada?: any) {
+    super(message, status, metatada);
+  }
 }
